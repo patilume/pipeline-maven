18e1db52f366546253709f464a4ec082ceeb6ec7
patilume <patilume@gmail.com>
2019-09-24 19:46:40 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "**************************************"
 
 WORKSPACE=jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -v var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm -v /var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
