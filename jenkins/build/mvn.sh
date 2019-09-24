d80558244969b6155640429ebad513e68c480c44
Umesh Patil <patilume@gmail.com>
2019-09-24 13:02:43 +0000

    fix workspace path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,4 +3,7 @@
 echo "**************************************"
 echo "**** Building JAR ******"
 echo "**************************************"
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+
+WORKSPACE=jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WOEKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
